5305258fc581bd02ecb6f2d6b2ef783a40b9c1f0
Uzz <uzmabagban56@gmail.com>
2021-09-27 14:36:10 +0530

    Add files via upload

---
+++ movies.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        items:4,
+        loop:true,
+        margin:20,
+        stagePadding:10,
+        autoplay:true,
+        autoplayTimeout:3000,
+        nav:false
+        
+    })
+  });
